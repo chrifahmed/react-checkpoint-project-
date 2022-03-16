1966e8b9d6dc024ee741fe2d85511bd926ed978c
whomadewhoandwhy@gmail.com <hard2carry@hotmail.com>
2022-03-16 17:02:06 +0100

    checkpoint

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+import { Navbar , Container , Nav , Form ,Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <>
+    
+    <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+    <div className='wraper Dub'>
+  <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+
+     </> 
+  );
+}
+
+export default App;
